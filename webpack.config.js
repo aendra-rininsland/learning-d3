1a7e73ae8f8168c9a035f5e85ebc964e6e72aee2
Ændrew Rininsland <aendrew@aendrew.com>
2015-11-01 02:10:41 +0000

    Initial -- HELLOOOOO WORLD!

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path');
+module.exports = {
+  entry: {app: ['./src/index.js']},
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/assets/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel'
+    }]
+  }
+};
