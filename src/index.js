ec519e1cc6cd1dee158da6ba20783cac552b10d2
Ændrew Rininsland <aendrew@aendrew.com>
2015-11-22 22:21:06 +0000

    TableBuilder example.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import {TableBuilder} from './table-builder';
 
+window.TableBuilder = TableBuilder;
+
 let header = ['one', 'two', 'three', 'four', 'five', 'six'];
+
 let rows = [
+  header,
   ['q', 'w', 'e', 'r', 't', 'y']
 ];
 
-let table = new TableBuilder([header, rows]);
+let table = new TableBuilder(rows);
 
 console.dir(table);
