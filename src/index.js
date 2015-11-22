e915b701022ceff09af726e90766150814ca0e58
Ændrew Rininsland <aendrew@aendrew.com>
2015-11-22 21:48:54 +0000

    Appending table element.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-import {BasicBarChart} from './basic-bar-chart';
+import {TableBuilder} from './table-builder';
 
-let data = require('./data/chapter1.json'); // Data from UNHCR, 2015.11.01: http://data.unhcr.org/api/population/regional.json
+let header = ['one', 'two', 'three', 'four', 'five', 'six'];
+let rows = [
+  ['q', 'w', 'e', 'r', 't', 'y']
+];
 
-let totalNumbers = data.filter((obj) => {return obj.population.length; })
-  .map((obj) => {
-    return {
-      name: obj.name,
-      population: Number(obj.population[0].value)
-    };
-  });
+let table = new TableBuilder([header, rows]);
 
-
-new BasicBarChart(totalNumbers);
-
-require('./index.css');
+console.dir(table);
