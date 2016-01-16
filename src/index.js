54c91209ada97f6ffa4f52bdb2a6e04668fea19e
Ændrew Rininsland <aendrew@aendrew.com>
2016-01-16 17:09:54 +0000

    Server part scaffolded.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 window.d3 = undefined;
 
-import PoliticalDonorChart from './chapter5';
+// import PoliticalDonorChart from './chapter5';
 // new PoliticalDonorChart('histogram');
 // new PoliticalDonorChart('pie', 'The Rt Hon Edward Miliband MP');
 // new PoliticalDonorChart('streamgraph');
@@ -10,4 +10,4 @@ import PoliticalDonorChart from './chapter5';
 // new PoliticalDonorChart('cluster', 'Miliband');
 // new PoliticalDonorChart('partition', 'Miliband');
 // new PoliticalDonorChart('pack');
-new PoliticalDonorChart('treemap', 'Miliband');
+// new PoliticalDonorChart('treemap', 'Miliband');
